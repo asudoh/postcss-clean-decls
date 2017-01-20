06d27aad79d1a3f0e5da12afdad78176819be7a8
Akira Sudoh <asudoh@gmail.com>
2017-01-21 01:10:51 +0900

    Introduce selector matching.

--- __tests__/index.js
+++ __tests__/index.js
@@ -11,43 +11,120 @@ async function run(input, output, opts) {
 
 it(`Should honor black list`, async () => {
   await run('.class { color: black; background-color: white; }', '.class { color: black; }', {
-    blacklist: 'background-color',
+    defs: [{ blacklist: 'background-color' }],
   });
 });
 
 it(`Should honor black list with regular expression`, async () => {
   await run('.class { color: black; background-color: white; }', '.class { color: black; }', {
-    blacklist: /^background/,
+    defs: [{ blacklist: /^background/ }],
   });
 });
 
 it(`Should honor black list in array`, async () => {
   await run('.class { color: black; background-color: white; }', '.class { }', {
-    blacklist: ['color', /^background/],
+    defs: [{ blacklist: ['color', /^background/] }],
   });
 });
 
 it(`Should honor white list`, async () => {
   await run('.class { color: black; background-color: white; }', '.class { background-color: white; }', {
-    whitelist: 'background-color',
+    defs: [{ whitelist: 'background-color' }],
   });
 });
 
 it(`Should honor white list with regular expression`, async () => {
   await run('.class { color: black; background-color: white; }', '.class { background-color: white; }', {
-    whitelist: /^background/,
+    defs: [{ whitelist: /^background/ }],
   });
 });
 
 it(`Should honor white list in array`, async () => {
   await run('.class { color: black; background-color: white; }', '.class { color: black; background-color: white; }', {
-    whitelist: ['color', /^background/],
+    defs: [{ whitelist: ['color', /^background/] }],
   });
 });
 
 it(`Should put white list bigger priority than black list`, async () => {
   await run('.class { color: black; background-color: white; }', '.class { color: black; }', {
-    blacklist: ['color', /^background/],
-    whitelist: /^color$/,
+    defs: [{
+      blacklist: ['color', /^background/],
+      whitelist: /^color$/,
+    }],
+  });
+});
+
+it(`Should process if the selector is in the include list`, async () => {
+  await run('.foo, .bar { background-color: white; }', '.foo, .bar { }', {
+    defs: [{
+      includes: '.foo',
+      blacklist: 'background-color',
+    }],
+  });
+});
+
+it(`Shouldn't process if the selector isn't in the include list`, async () => {
+  await run('.foo, .bar { background-color: white; }', '.foo, .bar { background-color: white; }', {
+    defs: [{
+      includes: '.class',
+      blacklist: 'background-color',
+    }],
+  });
+});
+
+it(`Shouldn't process if the selector is in the exclude list`, async () => {
+  await run('.foo, .bar { background-color: white; }', '.foo, .bar { background-color: white; }', {
+    defs: [{
+      excludes: '.foo',
+      blacklist: 'background-color',
+    }],
+  });
+});
+
+it(`Should process if the selector isn't in the exclude list`, async () => {
+  await run('.foo, .bar { background-color: white; }', '.foo, .bar { }', {
+    defs: [{
+      excludes: '.class',
+      blacklist: 'background-color',
+    }],
+  });
+});
+
+it(`Should put exclude list bigger priority than include list`, async () => {
+  await run('.foo, .bar { background-color: white; }', '.foo, .bar { background-color: white; }', {
+    defs: [{
+      includes: ['.foo', '.bar'],
+      excludes: '.foo',
+      blacklist: 'background-color',
+    }],
+  });
+});
+
+it(`Should process only the rules matching selectors`, async () => {
+  await run('.foo, .bar { background-color: white; } .foo { background-color: white; }',
+    '.foo, .bar { background-color: white; } .foo { }',
+    {
+      defs: [
+        {
+          includes: '.foo',
+          excludes: '.bar',
+          blacklist: 'background-color',
+        },
+      ],
+    });
+});
+
+it(`Should bail at the first match of the rule`, async () => {
+  await run('.class { color: black; background-color: white; }', '.class { color: black; }', {
+    defs: [
+      {
+        includes: /^\.c/i,
+        blacklist: 'background-color',
+      },
+      {
+        includes: '.class',
+        blacklist: 'color',
+      },
+    ],
   });
 });
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict';
 
 const postcss = require('postcss');
+const parser = require('postcss-selector-parser');
+
+function toArray(src) {
+  return !src || Array.isArray(src) ? src : [src];
+}
 
 function test(rule, str) {
   return rule && typeof rule.test === 'function' ? rule.test(str) : rule === str;
@@ -8,22 +13,42 @@ function test(rule, str) {
 
 // eslint-disable-next-line prefer-arrow-callback
 module.exports = postcss.plugin('clean-decls', function postCssPluginCleanDecls(options) {
-  const blacklist = !options.blacklist || Array.isArray(options.blacklist) ? options.blacklist : [options.blacklist];
-  const whitelist = !options.whitelist || Array.isArray(options.whitelist) ? options.whitelist : [options.whitelist];
+  if (!options.defs) {
+    throw new TypeError('Missing rules.');
+  }
   return function cleanDecls(css) {
-    if (blacklist) {
-      blacklist.forEach((blacklistItem) => {
-        css.walkDecls(blacklistItem, (decl) => {
-          if (!whitelist || !whitelist.some(whitelistItem => test(whitelistItem, decl.prop))) {
-            decl.remove();
+    css.walkRules((rule) => {
+      options.defs.some((def) => {
+        const includes = toArray(def.includes);
+        const excludes = toArray(def.excludes);
+        const blacklist = toArray(def.blacklist);
+        const whitelist = toArray(def.whitelist);
+
+        let matches;
+        parser((selectors) => {
+          matches = (!includes || selectors.some(item => includes.some(include => test(include, item.toString().trim()))))
+            && (!excludes || selectors.every(item => excludes.every(exclude => !test(exclude, item.toString().trim()))));
+        }).process(rule.selector);
+
+        if (matches) {
+          if (blacklist) {
+            blacklist.forEach((blacklistItem) => {
+              rule.walkDecls(blacklistItem, (decl) => {
+                if (!whitelist || !whitelist.some(whitelistItem => test(whitelistItem, decl.prop))) {
+                  decl.remove();
+                }
+              });
+            });
           }
-        });
+          rule.walkDecls((decl) => {
+            if (whitelist && !whitelist.some(whitelistItem => test(whitelistItem, decl.prop))) {
+              decl.remove();
+            }
+          });
+        }
+
+        return matches;
       });
-    }
-    css.walkDecls((decl) => {
-      if (whitelist && !whitelist.some(whitelistItem => test(whitelistItem, decl.prop))) {
-        decl.remove();
-      }
     });
   };
 });
